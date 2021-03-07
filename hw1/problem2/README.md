f1faa7fa5f0d4eeb484951ee255af567ff1029d4
GUL SENA ALTINTAS <galtintas17@ku.edu.tr>
2021-03-07 14:07:14 +0300

    Fixed issues in pipe example, problem 2

---
+++ hw1/problem2/README.md
@@ -0,0 +1,5 @@
+## Problem 2
+Gül Sena Altıntaş - 64284
+- Run `make` to compile code.
+- Run the program with `./problem2`
+- Clean with `make clean`
--- hw1/problem2/problem2.c
+++ hw1/problem2/problem2.c
@@ -1,55 +1,123 @@
-/*
-Gül Sena Altıntaş, 64284
-Hw 1, Problem 2
-calls fork operation 4 times and prints the id and parent id for each process call
-*/
+/**
+ * Example program demonstrating UNIX pipes.
+ *
+ * Figures 3.25 & 3.26
+ *
+ * @author Silberschatz, Galvin, and Gagne
+ * Operating System Concepts  - Ninth Edition
+ * Copyright John Wiley & Sons - 2013
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#include <string.h>
 #include <sys/time.h>
+#include <sys/wait.h>
+#include <time.h>
 
-void subtask(){
-    
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int send_to_pipe(int fd[2], struct timeval current_time, char write_msg[BUFFER_SIZE])
+{ /* parent process */
+    /* close the unused end of the pipe */
+    close(fd[READ_END]);
+
+    gettimeofday(&current_time, NULL);
+    // sprintf(write_msg, "%ld", current_time.tv_sec);
+    strftime(write_msg, BUFFER_SIZE, "%Y-%m-%d %H:%M:%S", localtime(&current_time.tv_sec));
+
+    //wait(NULL); this will block the child and the program won't terminate because
+    //child reading from the pipe blocks it until something is written to the pipe.
+    /* write to the pipe */
+    write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
+
+    /* close the write end of the pipe */
+    close(fd[WRITE_END]);
+
+    return 0;
 }
 
-int main()
-{
-    struct timeval current_time;
-    pid_t pid;
-    pid = fork();
-    if (pid == 0)
-    {
-        // process B
-        gettimeofday(&current_time, NULL);
-        printf("For Process B, PId: %ld, Current time in seconds: %ld\n", (long)getpid(), current_time.tv_sec);
-        sleep(3);
-        pid = fork();
+void read_to_pipe(int fd[2], char read_msg[BUFFER_SIZE], char process)
+{ /* child process */
+    /* close the unused end of the pipe */
+    close(fd[WRITE_END]);
 
-        if (pid == 0)
+    /* read from the pipe */
+    read(fd[READ_END], read_msg, BUFFER_SIZE);
+    printf("%c with PID %ld (parent id: %ld) read %s\n", process, (long)getpid(), (long)getppid(), read_msg);
+
+    /* close the write end of the pipe */
+    close(fd[READ_END]);
+}
+
+int fork_(int fd[2], int fd2[2], struct timeval current_time, char write_msg[BUFFER_SIZE], char read_msg[BUFFER_SIZE], char B)
+{
+    /* create the pipe */
+    if (pipe(fd) == -1)
+    {
+        fprintf(stderr, "Pipe failed");
+        return 1;
+    }
+
+    if (pipe(fd2) == -1)
+    {
+        fprintf(stderr, "Pipe failed");
+        return 1;
+    }
+    /* now fork a child process */
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        fprintf(stderr, "Fork failed");
+        return 1;
+    }
+
+    if (pid > 0)
+    {
+        send_to_pipe(fd, current_time, write_msg);
+        if (B == 'B')
         {
-            gettimeofday(&current_time, NULL);
-            printf("For Process C, PId: %ld, Current time in seconds: %ld\n", (long)getpid(), current_time.tv_sec);
-            sleep(3);
+            read_to_pipe(fd2, read_msg, 'A');
+            kill(pid, SIGKILL);
         }
         else
         {
-            wait(NULL);
-            gettimeofday(&current_time, NULL);
-            printf("For Process B, PId: %ld, Current time in seconds: %ld\n", (long)getpid(), current_time.tv_sec);
-            sleep(3);
-            printf("%ld\n", (long)pid);
+            read_to_pipe(fd2, read_msg, 'B');
             kill(pid, SIGKILL);
         }
     }
     else
     {
-        // parent process A
-        wait(NULL);
-        gettimeofday(&current_time, NULL);
-        printf("For Process A, PId: %ld, Current time in seconds: %ld\n", (long)getpid(), current_time.tv_sec);
-        printf("%ld\n", (long)pid);
-        kill(pid, SIGKILL);
+        // send_to_pipe(fd, current_time, write_msg);
+        // B reads and prints the time
+        read_to_pipe(fd, read_msg, B);
+        if (B == 'B')
+        {
+            sleep(3);
+            // B forks child process C
+            int fd_new[2];
+            int fd_new2[2];
+            fork_(fd_new, fd_new2, current_time, write_msg, read_msg, 'C');
+        }
+        sleep(3);
+        send_to_pipe(fd2, current_time, write_msg);
     }
+    return 0;
+}
+
+int main(void)
+{
+    char write_msg[BUFFER_SIZE];
+    char read_msg[BUFFER_SIZE];
+    pid_t pid;
+    int fd[2];
+    int fd2[2];
+    struct timeval current_time;
+
+    fork_(fd, fd2, current_time, write_msg, read_msg, 'B');
     return 0;
 }
